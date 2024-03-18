fd24efa265efd9b7b9c11acd166d025147ebc097
aaadam3042 <69839172+aaadam3042@users.noreply.github.com>
2024-03-18 17:37:02 +1100

    Basic hangman functionality working

--- app/src/main.rs
+++ app/src/main.rs
@@ -12,13 +12,53 @@ fn main() {
     let lines = word.lines().count();
 
     let random_number = rand::thread_rng().gen_range(1..=lines);
-    let chosen_word = word.lines().nth(random_number).unwrap();
+    let chosen_word = word.lines().nth(random_number).unwrap().to_lowercase();
 
     println!("{} - {}", random_number, chosen_word);
 
     let mut guessed_letters: Vec<char> = Vec::new();
-    let total_attempts = 6;
+    let total_attempts = 8; // Minimum attempts should be 8 - can make less if we skip frames
     let mut attempt = 0;
+    let mut guessed_word = "_".repeat(chosen_word.len());
+
+    let mut guessing = true;
+
+    while guessing && attempt < total_attempts {
+        println!("Word: {:?}", guessed_word);
+        let guess = utils::get_input("Guess a letter: ".to_string());
+        // Check if guess is a single char
+        if guess.len() > 1 {
+            println!("Please enter a single letter");
+            continue;
+        }
+        let guess_c = guess.chars().next().unwrap();    // TODO: Need to handle empty char - implement to only accept letters
+        
+        // Check if the letter has already been guessed
+        if guessed_letters.contains(&guess_c) {
+            println!("You've already guessed that letter");
+            continue;
+        }
+
+        // Check if the letter is in the word
+        if chosen_word.contains(guess_c) {
+            println!("Correct!");
+            chosen_word.chars().enumerate().for_each(|(i, c)| {
+                if c == guess_c {
+                    guessed_word.replace_range(i..i+1, &c.to_string());
+                }
+            });
+        } else {
+            println!("Incorrect!");
+            attempt += 1;
+            println!("Guesses left: {}", total_attempts - attempt);
+        }
+        guessed_letters.push(guess_c);
+
+        if chosen_word == guessed_word {
+            println!("Congratulations! You've guessed the word: {}", chosen_word);
+            guessing = false;
+        }
+    }
 
     // Every time the player guesses a letter add to guessed letters and display
     // Also animation frame should change every time the player guesses a letter with a modification to meet total attempts
